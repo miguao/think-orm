26c9d51cbd18b243a9824a4a6c32fa68d50ec3b2
konsn <1533818954@qq.com>
2025-07-25 11:57:33 +0800

    feat: 新增机构管理

---
+++ doubee-gateway/app/controller/admin/api/institution/InstitutionController.php
@@ -0,0 +1,61 @@
+<?php
+
+declare (strict_types=1);
+
+namespace app\controller\admin\api\institution;
+
+use app\controller\AbstractAdminController;
+use app\entity\database\Delete;
+use app\entity\database\Get;
+use app\entity\database\Save;
+use app\exception\JsonException;
+use app\middleware\admin\AuthenticationMiddleware;
+use app\model\SystemInstitution;
+use app\utils\DateUtils;
+use Exception;
+use think\annotation\route\Group;
+use think\annotation\route\Middleware;
+use think\annotation\route\Route;
+use think\response\Json;
+
+#[Group("/admin/api/institution")]
+#[Middleware(AuthenticationMiddleware::class)]
+class InstitutionController extends AbstractAdminController
+{
+    #[Route("GET", "getInstitutionList")]
+    public function getInstitutionList(): Json
+    {
+        $map = $this->request->get();
+        $get = new Get(SystemInstitution::class);
+        $get->setWhere((array)$map);
+        $get->setPaginate((int)$this->request->get("page"), (int)$this->request->get("limit"));
+        $get->setColumn("id", "email", "login_time", "last_login_time", "login_ip", "last_login_ip", "login_ua", "last_login_ua", "creation_time", "status");
+        $data = $this->database->get($get);
+
+        return $this->json(data: $data);
+    }
+
+    #[Route("*", "saveInstitution")]
+    public function saveInstitution(): Json
+    {
+        $map = $this->request->post();
+        $save = new Save(SystemInstitution::class);
+        $save->setMap($map);
+        $save->addForceMap("creation_time", DateUtils::current());
+        try {
+            $this->database->save($save);
+        } catch (Exception $exception) {
+            throw new JsonException("保存失败，错误：" . $exception->getMessage());
+        }
+
+        return $this->json(message: "保存成功");
+    }
+
+    #[Route("DELETE", "deleteInstitution")]
+    public function deleteInstitution(): Json
+    {
+        $delete = new Delete(SystemInstitution::class, (array)$this->request->post("list"));
+        $this->database->delete($delete);
+        return $this->json(message: "删除成功");
+    }
+}
---
+++ doubee-gateway/app/model/SystemInstitution.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\model;
+
+use think\Model;
+
+/**
+ * @property int $id
+ * @property int $parent_id
+ * @property string $name
+ * @property string $full_name
+ * @property string $code
+ * @property int $type
+ * @property int $sort
+ * @property string $creation_time
+ * @property int $status
+ */
+class SystemInstitution extends Model
+{
+
+}
