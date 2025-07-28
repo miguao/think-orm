c8871fe810c7d46ca0fbd16aba3601d1a25c9ca1
konsn <1533818954@qq.com>
2025-07-29 01:41:13 +0800

    fix: 取消机构分页查询

--- doubee-gateway/app/controller/admin/api/institution/InstitutionController.php
+++ doubee-gateway/app/controller/admin/api/institution/InstitutionController.php
@@ -28,7 +28,6 @@ class InstitutionController extends AbstractAdminController
         $map = $this->request->get();
         $get = new Get(SystemInstitution::class);
         $get->setWhere((array)$map);
-        $get->setPaginate((int)$this->request->get("page"), (int)$this->request->get("limit"));
         $get->setColumn("id", "parent_id", "name", "full_name", "code", "type", "sort", "creation_time", "status");
         $data = $this->database->get($get);
 
