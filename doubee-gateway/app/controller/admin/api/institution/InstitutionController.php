43923e783f9eb3d0a1a864e967577958046b21dd
konsn <1533818954@qq.com>
2025-07-29 01:41:55 +0800

    fix: 调整排序规格为降序

--- doubee-gateway/app/controller/admin/api/institution/InstitutionController.php
+++ doubee-gateway/app/controller/admin/api/institution/InstitutionController.php
@@ -28,6 +28,7 @@ class InstitutionController extends AbstractAdminController
         $map = $this->request->get();
         $get = new Get(SystemInstitution::class);
         $get->setWhere((array)$map);
+        $get->setOrderBy("sort", "desc");
         $get->setColumn("id", "parent_id", "name", "full_name", "code", "type", "sort", "creation_time", "status");
         $data = $this->database->get($get);
 
