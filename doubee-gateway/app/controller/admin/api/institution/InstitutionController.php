0e4c00eb5cda10f8169c104d75cbe3dfe3663220
konsn <1533818954@qq.com>
2025-07-29 15:12:18 +0800

    fix: 修复获取机构列表时的'id'排序逻辑，确保按'id'升序排列

--- doubee-gateway/app/controller/admin/api/institution/InstitutionController.php
+++ doubee-gateway/app/controller/admin/api/institution/InstitutionController.php
@@ -29,7 +29,7 @@ class InstitutionController extends AbstractAdminController
         $get = new Get(SystemInstitution::class);
         $get->setWhere((array)$map);
         $get->setOrderBy("sort", "desc");
-        $get->setOrderBy("id", "desc");
+        $get->setOrderBy("id", "asc");
         $get->setColumn("id", "parent_id", "name", "full_name", "code", "type", "sort", "creation_time", "status");
         $data = $this->database->get($get);
 
