fccc9e250119af9abcb94cb68ca769da5a4da55f
konsn <1533818954@qq.com>
2025-08-05 17:38:57 +0800

    fix: 更改接收字段名称为permissions

--- doubee-gateway/app/controller/admin/api/user/RoleController.php
+++ doubee-gateway/app/controller/admin/api/user/RoleController.php
@@ -45,7 +45,7 @@ class RoleController extends AbstractAdminController
         $map = $this->request->post();
         $save = new Save(SystemUserRole::class);
         $save->setMap($map);
-        $save->setMiddle("permission", SystemUserPermissionRelation::class, "permission_id", "role_id");
+        $save->setMiddle("permissions", SystemUserPermissionRelation::class, "permission_id", "role_id");
         $save->addForceMap("creation_time", DateUtils::current());
 
         try {
