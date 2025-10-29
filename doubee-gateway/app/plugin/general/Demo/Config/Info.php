385c6e2850202d534ae7f282ca941d48500c83e7
想 <6194318@qq.com>
2025-10-29 20:28:12 +0800

    feat: 新增通用演示插件

---
+++ doubee-gateway/app/plugin/general/Demo/Config/Info.php
@@ -0,0 +1,11 @@
+<?php
+
+declare (strict_types=1);
+
+return [
+    'name' => 'DEMO插件',
+    'author' => '官方',
+    'description' => '这是一个官方演示插件',
+    'version' => '1.0.0',
+    'type' => 'general',
+];
