e4addbcf8af767e423a773f5a654c5babc0d769b
想 <6194318@qq.com>
2025-10-31 16:35:21 +0800

    feat: 新增支付插件抽象基类

--- doubee-gateway/app/plugin/AliPay/Handler/Payment.php
+++ doubee-gateway/app/plugin/AliPay/Handler/Payment.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace app\plugin\AliPay\Handler;
 
+use app\kernel\plugin\abstract\AbstractPayment;
 use app\plugin\AliPay\Service\TradeService;
 
-class Payment
+class Payment extends AbstractPayment
 {
     public function __construct(protected TradeService $tradeService)
     {
