d19b805974ca40590565f381460be25db1ee4970
想 <6194318@qq.com>
2025-10-31 16:31:10 +0800

    feat: 新增微信支付插件配置文件

---
+++ doubee-gateway/app/plugin/WeChatPay/Config/Info.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use app\kernel\plugin\constant\Plugin;
+
+return [
+    Plugin::NAME => '微信支付',
+    Plugin::DESCRIPTION => "微信支付是腾讯公司的支付业务品牌，微信支付商户平台支持线下场所、公众号、小程序、PC网站、APP、企业微信等经营场景快速接入微信支付。微信支付全面打通O2O生活消费领域，提供专业的互联网+行业解决方案，微信支付支持微信红包和微信理财通，是移动支付的首选。",
+    Plugin::AUTHOR => '官方',
+    Plugin::VERSION => '1.0.0',
+    Plugin::TYPE => 'payment',
+];
