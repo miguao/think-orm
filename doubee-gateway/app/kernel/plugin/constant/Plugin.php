85149f906997f152f37622f281cba85130007c50
想 <6194318@qq.com>
2025-10-31 16:09:40 +0800

    feat: 新增插件辅助常量

---
+++ doubee-gateway/app/kernel/plugin/constant/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+declare (strict_types=1);
+
+namespace app\kernel\plugin\constant;
+
+interface Plugin
+{
+    const NAME = 'name';
+    const DESCRIPTION = 'description';
+    const AUTHOR = 'author';
+    const VERSION = 'version';
+    const TYPE = 'type';
+}
