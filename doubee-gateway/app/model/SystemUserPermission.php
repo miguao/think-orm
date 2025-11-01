511676b27dbe920de041e73a7aea8783975e841c
想 <6194318@qq.com>
2025-11-02 02:51:01 +0800

    fix: 更新系统用户权限模型字段注释

--- doubee-gateway/app/model/SystemUserPermission.php
+++ doubee-gateway/app/model/SystemUserPermission.php
@@ -8,8 +8,20 @@ use think\Model;
 
 /**
  * @property int $id
+ * @property int $parent_id
+ * @property string $icon
+ * @property string $name
+ * @property string $path
+ * @property string $component
+ * @property string $authority
+ * @property int $type
+ * @property int $open_type
+ * @property int $hide
+ * @property int $sort
+ * @property array $metadata
+ * @property string $creation_time
+ * @property int $status
  */
 class SystemUserPermission extends Model
 {
-
 }
