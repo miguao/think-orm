4e47c9f0d375cecf334d07290377b8ce39d8cf56
想 <6194318@qq.com>
2025-11-03 02:49:54 +0800

    fix: 优化注解路由

--- doubee-gateway/config/console.php
+++ doubee-gateway/config/console.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 use app\kernel\database\command\ModelCommand;
+use app\kernel\route\command\RouteCacheCommand;
+use app\kernel\route\command\RouteClearCommand;
 
 return [
     'commands' => [
         'gen:model' => ModelCommand::class,
+        'route:cache' => RouteCacheCommand::class,
+        'route:clear' => RouteClearCommand::class,
     ],
 ];
