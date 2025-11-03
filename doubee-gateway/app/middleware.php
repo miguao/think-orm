72847f9c8f25d473dc2664d49f13d0ff0b245475
想 <6194318@qq.com>
2025-11-03 15:03:11 +0800

    fix: 移除全局请求缓存

--- doubee-gateway/app/middleware.php
+++ doubee-gateway/app/middleware.php
@@ -3,9 +3,7 @@
 declare(strict_types=1);
 
 use think\middleware\AllowCrossDomain;
-use think\middleware\CheckRequestCache;
 
 return [
-    CheckRequestCache::class,
     AllowCrossDomain::class,
 ];
