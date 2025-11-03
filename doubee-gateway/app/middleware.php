8f5dcc991ce89cef9b0802ce11beec7f65984785
想 <6194318@qq.com>
2025-11-03 15:01:30 +0800

    feat: 新增全局请求缓存

--- doubee-gateway/app/middleware.php
+++ doubee-gateway/app/middleware.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use think\middleware\AllowCrossDomain;
+use think\middleware\CheckRequestCache;
 
 return [
+    CheckRequestCache::class,
     AllowCrossDomain::class,
 ];
