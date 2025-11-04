8dadc9251b49294c5606e524b23753f1049b73e9
想 <6194318@qq.com>
2025-11-04 18:06:27 +0800

    feat: 新增流式输出

--- doubee-gateway/app/controller/admin/api/plugin/PluginController.php
+++ doubee-gateway/app/controller/admin/api/plugin/PluginController.php
@@ -10,7 +10,9 @@ use app\kernel\plugin\PluginFactory;
 use app\kernel\route\annotation\Controller;
 use app\kernel\route\annotation\GetMapping;
 use app\kernel\route\annotation\Middleware;
+use app\kernel\route\annotation\PostMapping;
 use app\middleware\admin\AuthenticationMiddleware;
+use JetBrains\PhpStorm\NoReturn;
 use think\response\Json;
 
 #[Controller("/admin/api/plugin")]
@@ -28,6 +30,44 @@ class PluginController extends AbstractAdminController
 
     #[GetMapping("previewIcon")]
     public function previewIcon()
+    {
+
+    }
+
+    #[NoReturn]
+    #[PostMapping("startup")]
+    public function startup(): void
+    {
+        @ini_set('output_buffering', 'off');
+        @ini_set('zlib.output_compression', false);
+        header('Content-Type: text/plain; charset=utf-8');
+        header('Cache-Control: no-cache');
+        header('X-Accel-Buffering: no');
+        header('Access-Control-Allow-Origin: *');
+
+        $path = escapeshellarg(root_path());
+        $cmd = "cd {$path} && composer require guzzlehttp/guzzle -vvv";
+        $process = popen($cmd . " 2>&1", 'r');
+
+        ob_flush();
+        flush();
+
+        while (!feof($process)) {
+            $line = fgets($process);
+            if ($line !== false) {
+                echo $line;
+                ob_flush();
+                flush();
+            }
+        }
+
+        pclose($process);
+        exit();
+    }
+
+
+    #[PostMapping("stop")]
+    public function stop()
     {
 
     }
