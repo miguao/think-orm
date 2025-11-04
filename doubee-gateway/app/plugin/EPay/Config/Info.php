c1541a381f3a7bad4eb6526a5a888d268a2de654
想 <6194318@qq.com>
2025-11-04 10:36:35 +0800

    feat: 新增彩虹易支付插件

---
+++ doubee-gateway/app/plugin/EPay/Config/Info.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use app\kernel\plugin\constant\Plugin;
+
+return [
+    Plugin::NAME => '彩虹易支付',
+    Plugin::DESCRIPTION => "解决个人支付难题，一站式聚合接入支付宝,微信,QQ钱包,银联,APP,小程序支付，帮助开发者快速集成到自己相应产品，效率高，见效快，费率低。",
+    Plugin::AUTHOR => '官方',
+    Plugin::VERSION => '1.0.0',
+    Plugin::TYPE => 'payment',
+];
---
+++ doubee-gateway/app/plugin/EPay/Handler/Payment.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\plugin\EPay\Handler;
+
+use app\kernel\plugin\abstract\AbstractPayment;
+
+class Payment extends AbstractPayment
+{
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function async()
+    {
+        // TODO: Implement async() method.
+    }
+}
