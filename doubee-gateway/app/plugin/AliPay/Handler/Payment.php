e1ea002b7e55f10aa64db26db4f121052a1d3449
想 <6194318@qq.com>
2025-11-06 17:17:24 +0800

    fix: 优化支付宝插件

--- doubee-gateway/app/plugin/AliPay/Handler/Payment.php
+++ doubee-gateway/app/plugin/AliPay/Handler/Payment.php
@@ -5,17 +5,11 @@ declare(strict_types=1);
 namespace app\plugin\AliPay\Handler;
 
 use app\kernel\plugin\abstract\AbstractPayment;
-use app\plugin\AliPay\Service\TradeService;
 
 class Payment extends AbstractPayment
 {
-    public function __construct(protected TradeService $tradeService)
-    {
-    }
-
     public function create()
     {
-        return $this->tradeService->preCreate();
     }
 
     public function async()
