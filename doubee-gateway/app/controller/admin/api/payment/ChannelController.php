845e5d46ddbd6910c96a5f212126922c6604740a
想 <6194318@qq.com>
2025-11-07 14:47:28 +0800

    feat: 新增保存通道

--- doubee-gateway/app/controller/admin/api/payment/ChannelController.php
+++ doubee-gateway/app/controller/admin/api/payment/ChannelController.php
@@ -6,9 +6,11 @@ namespace app\controller\admin\api\payment;
 
 use app\controller\AbstractAdminController;
 use app\entity\database\Get;
+use app\entity\database\Save;
 use app\kernel\route\annotation\Controller;
 use app\kernel\route\annotation\GetMapping;
 use app\kernel\route\annotation\Middleware;
+use app\kernel\route\annotation\RequestMapping;
 use app\middleware\admin\AuthenticationMiddleware;
 use app\model\PaymentChannel;
 use think\db\Query;
@@ -30,5 +32,17 @@ class ChannelController extends AbstractAdminController
         });
 
         return $this->json(data: $data);
+    }
+
+    #[RequestMapping("saveChannel", ["POST", "PUT"])]
+    public function saveChannel(): Json
+    {
+        $map = $this->request->post();
+        $save = new Save(PaymentChannel::class);
+        $save->setMap($map);
+        $save->enableCreateTime();
+
+        $this->database->save($save);
+        return $this->json(message: "保存成功");
     }
 }
