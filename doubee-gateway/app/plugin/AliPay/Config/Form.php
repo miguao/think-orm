c4fa3e6d2375a8e2a514bc183058fb74d8775ac5
想 <6194318@qq.com>
2025-11-08 17:56:01 +0800

    feat: 新增表单组件

---
+++ doubee-gateway/app/plugin/AliPay/Config/Form.php
@@ -0,0 +1,26 @@
+<?php
+
+declare (strict_types=1);
+
+return [
+    [
+        [
+            'title' => '应用ID',
+            'name' => 'app_id',
+            'type' => 'input',
+            'placeholder' => '请输入应用ID',
+        ],
+        [
+            'title' => '应用私钥',
+            'name' => 'apply_private_key',
+            'type' => 'textarea',
+            'placeholder' => '请输入应用私钥',
+        ],
+        [
+            'title' => '支付宝公钥',
+            'name' => 'alipay_public_key',
+            'type' => 'textarea',
+            'placeholder' => '请输入支付宝公钥',
+        ],
+    ]
+];
