ef2ea93cd281d8dad2427b19080268920d929fb7
想 <6194318@qq.com>
2025-11-08 18:11:39 +0800

    feat: 新增插件示例

---
+++ doubee-gateway/app/plugin/Demo/Config/Form.php
@@ -0,0 +1,20 @@
+<?php
+
+declare (strict_types=1);
+
+return [
+    [
+        [
+            'title' => '输入框组件',
+            'name' => 'test_input',
+            'type' => 'input',
+            'placeholder' => '这是一个输入框说明',
+        ],
+        [
+            'title' => '文本域组件',
+            'name' => 'test_textarea',
+            'type' => 'textarea',
+            'placeholder' => '请输入这是一个文本域说明',
+        ],
+    ]
+];
---
+++ doubee-gateway/app/plugin/EPay/Config/Form.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    [
+        [
+            'title' => '商户ID',
+            'name' => 'merchant_id',
+            'type' => 'input',
+            'placeholder' => '请输入商户ID',
+        ],
+        [
+            'title' => '商户密钥',
+            'name' => 'merchant_key',
+            'type' => 'input',
+            'placeholder' => '请输入商户密钥',
+        ],
+        [
+            'title' => '网关地址',
+            'name' => 'gateway',
+            'type' => 'input',
+            'placeholder' => '请输入网关地址',
+        ],
+    ]
+];
