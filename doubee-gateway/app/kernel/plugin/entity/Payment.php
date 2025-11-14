80e51475e234e7c8db4a733e6d5db47bde1b8753
想 <6194318@qq.com>
2025-11-14 17:45:48 +0800

    feat: 新增支付响应实体类

--- doubee-gateway/app/kernel/plugin/entity/Payment.php
+++ doubee-gateway/app/kernel/plugin/entity/Payment.php
@@ -7,4 +7,36 @@ namespace app\kernel\plugin\entity;
 class Payment
 {
     private int $renderMode;
+    private ?string $payUrl = null;
+    private array $options = [];
+
+    public function getRenderMode(): int
+    {
+        return $this->renderMode;
+    }
+
+    public function setRenderMode(int $renderMode): void
+    {
+        $this->renderMode = $renderMode;
+    }
+
+    public function getPayUrl(): string
+    {
+        return $this->payUrl;
+    }
+
+    public function setPayUrl(string $payUrl): void
+    {
+        $this->payUrl = $payUrl;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function setOption(array $options): void
+    {
+        $this->options = $options;
+    }
 }
