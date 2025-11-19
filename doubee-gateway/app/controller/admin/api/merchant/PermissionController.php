ef9b3c2a9aedff9501f157aa912afb5a69d04d60
想 <6194318@qq.com>
2025-11-19 11:15:01 +0800

    feat: 新增商户权限保存权限、删除权限

--- doubee-gateway/app/controller/admin/api/merchant/PermissionController.php
+++ doubee-gateway/app/controller/admin/api/merchant/PermissionController.php
@@ -3,12 +3,19 @@
 namespace app\controller\admin\api\merchant;
 
 use app\controller\AbstractAdminController;
+use app\entity\database\Delete;
 use app\entity\database\Get;
+use app\entity\database\Save;
+use app\exception\JsonException;
 use app\kernel\route\annotation\Controller;
+use app\kernel\route\annotation\DeleteMapping;
 use app\kernel\route\annotation\GetMapping;
 use app\kernel\route\annotation\Middleware;
+use app\kernel\route\annotation\RequestMapping;
 use app\middleware\admin\AuthenticationMiddleware;
 use app\model\MerchantPermission;
+use app\utils\DateUtils;
+use Exception;
 use think\response\Json;
 
 #[Controller("/admin/api/merchant/permission")]
@@ -25,5 +32,29 @@ class PermissionController extends AbstractAdminController
         $data = $this->database->get($get);
 
         return $this->json(data: $data);
+    }
+
+    #[RequestMapping("savePermission", ["POST", "PUT"])]
+    public function savePermission(): Json
+    {
+        $map = $this->request->post();
+        $save = new Save(MerchantPermission::class);
+        $save->setMap($map);
+        $save->addForceMap("creation_time", DateUtils::current());
+        try {
+            $this->database->save($save);
+        } catch (Exception $exception) {
+            throw new JsonException("保存失败，错误：" . $exception->getMessage());
+        }
+
+        return $this->json(message: "保存成功");
+    }
+
+    #[DeleteMapping("deletePermission")]
+    public function deletePermission(): Json
+    {
+        $delete = new Delete(MerchantPermission::class, (array)$this->request->post("list"));
+        $this->database->delete($delete);
+        return $this->json(message: "删除成功");
     }
 }
