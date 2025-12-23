11112df27db24fb360a12b8792d6829946c1b8cb
想 <6194318@qq.com>
2025-12-24 02:40:01 +0800

    fix: 临时屏蔽部分组件功能

--- doubee-merchant/src/layout/index.vue
+++ doubee-merchant/src/layout/index.vue
@@ -74,14 +74,14 @@
         </el-icon>
       </layout-tool>
       <!-- 刷新 -->
-      <layout-tool
+      <!-- <layout-tool
         :class="{ 'hidden-sm-and-down': tabBar && tabInHeader }"
         @click="reloadPageTab()"
       >
         <el-icon style="transform: scale(1.09)">
           <ReloadOutlined />
         </el-icon>
-      </layout-tool>
+      </layout-tool> -->
     </template>
     <!-- 顶栏右侧按钮 -->
     <template #right>
@@ -120,11 +120,11 @@
         />
       </layout-tool>
       <!-- 主题设置 -->
-      <layout-tool @click="openSetting">
+      <!-- <layout-tool @click="openSetting">
         <el-icon>
           <MoreOutlined />
         </el-icon>
-      </layout-tool>
+      </layout-tool> -->
     </template>
     <!-- 页签栏右侧下拉菜单 -->
     <template v-if="tabBar && !tabInHeader" #tabExtra="{ active }">
