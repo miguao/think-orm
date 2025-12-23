6695205952f78a741a0defe811f38b941b91570b
想 <6194318@qq.com>
2025-12-24 02:36:43 +0800

    fix: 调整支付宝插件绑定关系

--- doubee-gateway/app/plugin/AliPay/Config/Dependencies.php
+++ doubee-gateway/app/plugin/AliPay/Config/Dependencies.php
@@ -2,9 +2,4 @@
 
 declare(strict_types=1);
 
-use app\plugin\AliPay\Service\Impl\TradeServiceImpl;
-use app\plugin\AliPay\Service\TradeService;
-
-return [
-    TradeService::class => TradeServiceImpl::class,
-];
+return [];
