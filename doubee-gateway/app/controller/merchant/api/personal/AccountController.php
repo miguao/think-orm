abd0a0abcefb11e01fcd7fc61aa71c3a823ccca7
想 <6194318@qq.com>
2025-12-24 02:34:54 +0800

    fix: 调整商户中心菜单字段

--- doubee-gateway/app/controller/merchant/api/personal/AccountController.php
+++ doubee-gateway/app/controller/merchant/api/personal/AccountController.php
@@ -26,7 +26,7 @@ class AccountController extends AbstractMerchantController
     {
         $user = $this->getUserInfo();
         $permissions = $this->userService->findByUserPermissions($this->getUserId());
-        $user['menus'] = $permissions['menus'];
+        $user['authorities'] = $permissions['menus'];
 
         return $this->json(data: $user);
     }
