81917092b4d2efb1414df34dffba75ec02201a83
想 <6194318@qq.com>
2026-01-03 23:23:48 +0800

    feat: 新增获取配置项字段列表

--- doubee-gateway/app/controller/admin/api/config/FieldController.php
+++ doubee-gateway/app/controller/admin/api/config/FieldController.php
@@ -5,18 +5,27 @@ declare(strict_types=1);
 namespace app\controller\admin\api\config;
 
 use app\controller\AbstractAdminController;
+use app\entity\database\Get;
 use app\kernel\route\annotation\Controller;
 use app\kernel\route\annotation\GetMapping;
 use app\kernel\route\annotation\Middleware;
 use app\middleware\admin\AuthenticationMiddleware;
+use app\model\SystemConfigField;
+use think\response\Json;
 
 #[Controller("/admin/api/config/field")]
 #[Middleware(AuthenticationMiddleware::class)]
 class FieldController extends AbstractAdminController
 {
     #[GetMapping("getFieldList")]
-    public function getFieldList()
+    public function getFieldList(): Json
     {
+        $map = $this->request->get();
+        $get = new Get(SystemConfigField::class);
+        $get->setWhere((array)$map);
+        $get->setPaginate((int)$this->request->get("page"), (int)$this->request->get("limit"));
+        $data = $this->database->get($get);
 
+        return $this->json(data: $data);
     }
 }
