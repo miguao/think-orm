f753c4432a8a665093dfabe7b82dfbbe7e344e00
想 <6194318@qq.com>
2026-01-09 09:56:28 +0800

    fix: 调试定时任务

--- doubee-gateway/app/job/OrderNotificationJob.php
+++ doubee-gateway/app/job/OrderNotificationJob.php
@@ -9,6 +9,7 @@ use app\model\PaymentOrder;
 use app\utils\StringUtils;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
+use think\facade\Log;
 use think\queue\Job;
 use Throwable;
 
@@ -22,17 +23,24 @@ class OrderNotificationJob implements JobInterface
 
     public function fire(Job $job, array $data): void
     {
+        Log::info("进入队列第一步");
+
         $attempts = $job->attempts();
         if ($attempts > $this->maxAttempts) {
+            Log::info("进入队列第二步，超过最大重试次数");
+
             $job->delete();
             return;
         }
 
+        Log::info("进入队列第三步，查询订单信息");
         $order = PaymentOrder::query()
             ->with(['application', 'type', 'merchant'])
             ->find($data['id']);
 
         if (!$order || empty($order->notification_url)) {
+            Log::info("进入队列第四步，数据异常，直接结束");
+
             // 数据异常，直接结束
             $job->delete();
             return;
@@ -52,15 +60,27 @@ class OrderNotificationJob implements JobInterface
         $params['sign'] = StringUtils::generateSignature($params, $order->merchant->merchant_key);
         $params['sign_type'] = 'MD5';
 
+        Log::info("进入队列第五步，拼接验签参数");
+
         try {
             $client = new Client(['verify' => false, 'timeout' => 10, 'http_errors' => false]);
+
+            Log::info("进入队列第六步，开始发送请求");
+
             $response = $client->post($data['notification_url'], [RequestOptions::FORM_PARAMS => $params]);
             $body = trim((string)$response->getBody());
+
+            Log::info("进入队列第七步，获取响应数据");
+
             if ($body === 'success') {
+                Log::info("进入队列第八步，更新订单状态");
+
                 $order->notification_status = 1;
                 $order->save();
 
                 $job->delete();
+
+                Log::info("进入队列第九步，结束通知");
                 return;
             }
 
@@ -68,6 +88,8 @@ class OrderNotificationJob implements JobInterface
         } catch (Throwable $throwable) {
             // 未成功，延迟重试
             $delay = $this->retryDelay($attempts);
+
+            Log::info("进入队列第十步，延迟重试");
 
             $job->release($delay);
         }
