449e906a2faa6c4df3545fe9f018a4911d958ddb
想 <6194318@qq.com>
2026-01-09 10:11:37 +0800

    fix: 调试定时任务

--- doubee-gateway/app/job/OrderNotificationJob.php
+++ doubee-gateway/app/job/OrderNotificationJob.php
@@ -38,6 +38,8 @@ class OrderNotificationJob implements JobInterface
             ->with(['application', 'type', 'merchant'])
             ->find($data['id']);
 
+        Log::info("进入队列第三ban步，查到订单数据：" . json_encode($order));
+
         if (!$order || empty($order->notification_url)) {
             Log::info("进入队列第四步，数据异常，直接结束");
 
